19a68d299a1e576c499c17f17d22cb4b3cac2f88
Alexandru Tarna <tarna.alexandru@gmail.com>
2018-05-11 12:13:02 +0200

    included stevens libraries

--- lab2.3/source/server1/server1_main.c
+++ lab2.3/source/server1/server1_main.c
@@ -4,7 +4,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *prog_name;
+#include <errno.h>
+#include <ctype.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "../errlib.h"
+#include "../sockwrap.h"
+
 
 int main (int argc, char *argv[])
 {
